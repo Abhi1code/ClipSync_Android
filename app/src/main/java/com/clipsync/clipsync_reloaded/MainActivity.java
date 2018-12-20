bcfffe157b6328589cbfd5352c79573ca0a096d3
Abhi1code <mehariaabhishek@gmail.com>
2018-12-20 09:54:18 +0530

    Initial commit (0.1)

---
+++ app/src/main/java/com/clipsync/clipsync_reloaded/MainActivity.java
@@ -0,0 +1,13 @@
+package com.clipsync.clipsync_reloaded;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
